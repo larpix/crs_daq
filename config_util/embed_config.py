544f5f8d302e5269be38ac2878bb4fb292331a69
Matt Kramer <mjkramer@users.noreply.github.com>
2024-05-01 22:00:28 -0700

    Add --use-destination-dir to embed_config.py

--- config_util/embed_config.py
+++ config_util/embed_config.py
@@ -2,6 +2,7 @@
 
 import argparse
 import io
+import json
 from pathlib import Path
 import tarfile
 
@@ -16,6 +17,8 @@ def main():
     ap = argparse.ArgumentParser()
     ap.add_argument('hdf5_file', type=Path)
     ap.add_argument('config_dir', type=Path)
+    ap.add_argument('--use-destination-dir', action='store_true',
+                    help='Assume hdf5_file is a path relative to destination_dir_ from RUN_CONFIG.kson')
     args = ap.parse_args()
 
     buf = io.BytesIO()
@@ -23,7 +26,13 @@ def main():
         dirname = NAME + '.' + args.hdf5_file.stem
         tarf.add(args.config_dir, arcname=dirname)
 
-    with h5py.File(args.hdf5_file, 'a') as h5f:
+    hdf5_file = args.hdf5_file
+    if args.use_destination_dir:
+        conf = json.load(open('RUN_CONFIG.json'))
+        dest = conf['destination_dir_']
+        hdf5_file = f'{dest}/{args.hdf5_file}'
+
+    with h5py.File(hdf5_file, 'a') as h5f:
         data = np.frombuffer(buf.getvalue(), dtype=np.uint8)
         dset = h5f.create_dataset(NAME, data=data)
         # for convenience:
