0e870fccdf958789f54becc83909f3f52481026a
Dune <acdaq@acd-daq02.fnal.gov>
2026-07-01 21:12:15 -0500

    I tried improving networks on iog6 - no way, tile 5 is not improving, independent of voltage. Also, tile 3 has a very active chip on iochan 10 - i removed 43, but I believe it is actually 38 - plot_hot_chip.py appears to not be working anymore

--- config_util/enable_rolling_reset.py
+++ config_util/enable_rolling_reset.py
@@ -17,4 +17,4 @@ if __name__ == "__main__":
             print(f"File {path} not found, skipping")
             continue
         patch_config(path)
-        print(f"Patched {path}")
+        #print(f"Patched {path}")
